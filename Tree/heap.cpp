452e929d2a6f65a083a2ae08716f695d5de47bb0
unknown <siscos75@gmail.com>
2019-06-16 11:54:00 +0900

    modify heap

--- Tree/heap.cpp
+++ Tree/heap.cpp
@@ -14,3 +14,29 @@ void push_heap(vector<int>& heap, int newValue)
 		idx = (idx - 1) / 2;
 	}
 }
+
+// 정소를 담는 최대힙에서 heap[0]을 제거한다.
+void pop_heap(vector<int>& heap)
+{
+	// 힙의 맨 끝에서 값을 가져와서 루트에 덮어씌운다.
+	heap[0] = heap.back();
+	heap.pop_back();
+	int here = 0;
+	while(true)
+	{
+		int left =  idx * 2 + 1, right = idx * 2 + 2;
+		// 리프에 도달한 경우
+		if (left >= heap.size()) break;
+		// heap[here]가 내려갈 위치를 찾는다.
+		int next = here;
+		if (heap[next] < heap[left])
+			next = left;
+		
+		if (right < heap.size() && heap[next] < heap[right])
+			next = right;
+		
+		if (next == here) break;
+		swap(heap[here], heap[next]);
+		here = next;
+	}
+}
