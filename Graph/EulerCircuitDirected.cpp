3d97314dcf9dc955ea44a1a524b78a901fdaef12
unknown <siscos75@gmail.com>
2019-06-17 17:09:31 +0900

    dev

--- Graph/EulerCircuitDirected.cpp
+++ Graph/EulerCircuitDirected.cpp
@@ -3,11 +3,11 @@ void getEulerCircuit(int here, vector<int>& circuit)
 {
     for(int there = 0; there < adj[here].size(); ++there)
     {
-	while(adj[here][there] > 0)
-	{
-	    adj[here][there]--;//간선을 지운다
-	    getEulerCircuit(there, circuit);
-	}
+        while(adj[here][there] > 0)
+        {
+            adj[here][there]--;//간선을 지운다
+            getEulerCircuit(there, circuit);
+        }
     }
     circuit.push_back(here);
 }
@@ -19,20 +19,20 @@ vector<int> getEulerTrailOrCircuit()
     // 우선 트레일을 찾아본다: 시작점이 존재하는 경우
     for(int i = 0; i < outdegree.size(); i++)
     {
-	if (outdegree[i] == indegree[j] + 1)
-	{
-	    getEulerCircuit(i, circuit);
-	    return circuit;
-	}
+        if (outdegree[i] == indegree[j] + 1)
+        {
+            getEulerCircuit(i, circuit);
+            return circuit;
+        }
     }
 
     for(int i = 0; i < outdegree.size(); i++)
     {
-	if (outdegree[i])
-	{
-	    getEulerCircuit(i, circuit);
-	    return circuit;
-	}
+        if (outdegree[i])
+        {
+            getEulerCircuit(i, circuit);
+            return circuit;
+        }
     }
 
     return circuit;
